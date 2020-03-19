44718542f855198e7cae7a87625ca1c73a674fed
Denison de Alemida <denisondealmeidaprtela@hotmail.com>
2020-03-19 13:49:18 -0300

    Adicionei o java script para transicao na area de compras, e adicionei tambem a div com as informacoes das pizzas, porem nao estou conseguindo integrar o houver para dar um display:block; e display:none;

---
+++ assets/js/esconder.js
@@ -0,0 +1,39 @@
+var div1;
+var div2;
+var div3;
+
+window.onload = function(){
+	div1 = document.getElementById("meu_cardapio_vendas_salgado");
+	div2 = document.getElementById("meu_cardapio_vendas_doce");
+	div3 = document.getElementById("meu_cardapio_vendas_bebidas");
+
+	var bt1 = document.getElementById("bt1");
+	bt1.onclick = mostrarDiv1;
+	var bt2 = document.getElementById("bt2");
+	bt2.onclick = mostrarDiv2;
+	var bt3 = document.getElementById("bt3");
+	bt3.onclick = mostrarDiv3;
+
+	div1.classList.add("escondido");
+	div2.classList.add("escondido");
+	div3.classList.add("escondido");
+
+}
+
+function mostrarDiv1(){
+	div1.classList.remove("escondido");
+	div2.classList.add("escondido");
+	div3.classList.add("escondido");
+}
+function mostrarDiv2(){
+	div1.classList.add("escondido");
+	div2.classList.remove("escondido");
+	div3.classList.add("escondido");
+
+}
+function mostrarDiv3(){
+	div1.classList.add("escondido");
+	div2.classList.add("escondido");
+	div3.classList.remove("escondido");
+
+}
